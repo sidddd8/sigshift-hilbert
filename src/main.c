17090344383d6623093f9f72c8271508fdc947c4
sidddd8 <lsida806@gmail.com>
2023-11-14 22:52:50 +0100

    loading signal values from csv

--- src/main.c
+++ src/main.c
@@ -12,41 +12,74 @@
 
 typedef struct {
     int id;
+    int sig_len;
     int *reference;
     int *shifted;
 } sig_pair;
 
-void load_signals(sig_pair *signals, const char *file_path, int id) {
-    
+FILE *open_csv(const char *file_path) {
+    FILE *fd;
     char cwd[MAX_PATHLEN];
+
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
         fprintf(stderr, "ERR: Failed obtaining current working directory\n");
+        return NULL;
+    }
+    fd = fopen(strcat(cwd, file_path), "r");
+    if (fd == NULL) {
+        fprintf(stderr, "ERR: Failed opening file '%s'\n", file_path);
+        return NULL;
+    }
+    return fd;
+}
+
+int load_signals(sig_pair *signals, FILE *fd, int id) {
+    int size = 750;
+    signals->id = id;
+    signals->reference = calloc(size, sizeof(int));
+    signals->shifted = calloc(size, sizeof(int));
+    if ((signals->reference == NULL) || (signals->shifted == NULL)) {
+        fprintf(stderr, "ERR: Failed initial calloc for signal reference or shift\n");
+        return EXIT_FAILURE;
     }
 
-    FILE *fp;
     char row[MAX_LINELEN];
     char *token;
 
-    fp = fopen(strcat(cwd, file_path), "r");
-    if (fp == NULL) {
-        fprintf(stderr, "ERR: Failed opening file '%s'\n", file_path);
-        return;
-    }
-
-    while (feof(fp) != true) {
-        if (fgets(row, MAX_LINELEN, fp) == NULL) {
-            fprintf(stderr, "ERR: Failed reading a line in %s\n", file_path);
-            return;
+    int num_lines = 0;
+    while (feof(fd) != true) {
+        if (fgets(row, MAX_LINELEN, fd) == NULL) {
+            break;
         }
-        
-        fprintf(stderr, "row is: %s\n", row);
-        token = strtok(row, ";");
-        while(token != NULL)
-        {
-            printf("Token: %s\n", token);
-            token = strtok(NULL, ";");
+        if (num_lines >= size) {
+            size *= 2;
+            int *tmp_ref = realloc(signals->reference, size*sizeof(int));
+            int *tmp_shift = realloc(signals->shifted, size*sizeof(int));
+            if ((tmp_ref == NULL) || (tmp_shift == NULL)) {
+                fprintf(stderr, "ERR: Failed realloc for signal reference or shift\n");
+                return EXIT_FAILURE;
+            }
+            signals->reference = tmp_ref;
+            signals->shifted = tmp_shift;
         }
+        // first line (line 0) is the header of the .csv
+        if (num_lines >= 1) {
+            token = strtok(row, ";");
+            signals->reference[num_lines-1] = atoi(token);
+            
+            int tok_id = 1;
+            while(token != NULL) {
+                token = strtok(NULL, ";");
+                if (tok_id == id) {
+                    signals->shifted[num_lines-1] = atoi(token);
+                }
+                tok_id++;
+            }
+        }
+        num_lines++;
     }
+    signals->sig_len = num_lines-1;
+    return EXIT_SUCCESS;
 }
 
 char* parse_args(int argc, char *argv[]) {
@@ -65,10 +98,20 @@ int main(int argc, char *argv[]) {
     
     const char *file_path = parse_args(argc, argv);
     fprintf(stderr, "INFO: Working data file: '%s'\n", file_path);
+
+    FILE *fd = open_csv(file_path);
     sig_pair signals;
-    load_signals(&signals, file_path, SHIFTED_ID);
-    
-    
+
+    if (load_signals(&signals, fd, SHIFTED_ID) == EXIT_FAILURE) {
+        fprintf(stderr, "ERR: Failed loading singal values\n");
+    }
+    else {
+        fprintf(stderr, "INFO: Succesfuly read and loaded signal values\n");
+    }
+
+    free(signals.reference);
+    free(signals.shifted);
+    fclose(fd);
     
     return 0;
 }
