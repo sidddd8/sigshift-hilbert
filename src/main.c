b36a3a97b43e101ad79a99f6154a2f9329f71b61
sidddd8 <lsida806@gmail.com>
2023-11-14 18:34:03 +0100

    added reading .csv functionality

--- src/main.c
+++ src/main.c
@@ -1,15 +1,74 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define MAX_PATHLEN 400
+#define MAX_LINELEN 100
+#define DEFAULT_FILEPATH "/src/data/DIT-SHM-dataset.csv"
+#define SHIFTED_ID 1
+
+typedef struct {
+    int id;
+    int *reference;
+    int *shifted;
+} sig_pair;
+
+void load_signals(sig_pair *signals, const char *file_path, int id) {
+    
+    char cwd[MAX_PATHLEN];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        fprintf(stderr, "ERR: Failed obtaining current working directory\n");
+    }
+
+    FILE *fp;
+    char row[MAX_LINELEN];
+    char *token;
+
+    fp = fopen(strcat(cwd, file_path), "r");
+    if (fp == NULL) {
+        fprintf(stderr, "ERR: Failed opening file '%s'\n", file_path);
+        return;
+    }
+
+    while (feof(fp) != true) {
+        if (fgets(row, MAX_LINELEN, fp) == NULL) {
+            fprintf(stderr, "ERR: Failed reading a line in %s\n", file_path);
+            return;
+        }
+        
+        fprintf(stderr, "row is: %s\n", row);
+        token = strtok(row, ";");
+        while(token != NULL)
+        {
+            printf("Token: %s\n", token);
+            token = strtok(NULL, ";");
+        }
+    }
+}
+
+char* parse_args(int argc, char *argv[]) {
+    char *file_path;
+    if (argc == 1) {
+        file_path = DEFAULT_FILEPATH;
+    } 
+    else {
+        file_path = argv[1];
+    }
+    return file_path;
+}
+
 
 int main(int argc, char *argv[]) {
     
-    if (argc < 2) {
-        fprintf(stderr, "ERROR: Please specify a path do the dataset\n");
-        return -1;
-    } 
+    const char *file_path = parse_args(argc, argv);
+    fprintf(stderr, "INFO: Working data file: '%s'\n", file_path);
+    sig_pair signals;
+    load_signals(&signals, file_path, SHIFTED_ID);
     
-    const char *file_path = argv[1];
-    fprintf(stderr, "INFO: Data file specified: '%s'\n", file_path);
+    
     
     return 0;
 }
